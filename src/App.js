d462181c5e02fee4b76d884f22baaf227b966d03
skdileepvarma <dileepvarmamail@gmail.com>
2020-04-13 12:07:55 +0000

    changes to App text

--- src/App.js
+++ src/App.js
@@ -10,6 +10,9 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+	<p>
+  	Deepu = Jahnu = Mouni
+	</p>
         <a
           className="App-link"
           href="https://reactjs.org"
